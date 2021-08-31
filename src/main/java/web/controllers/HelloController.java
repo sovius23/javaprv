926e1f042d8bb7a55097dab3f146936a8de5ec73
sovius23 <sovius5@mail.ru>
2021-08-31 20:56:48 +0300

    start java

---
+++ src/main/java/web/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String greeting(Model model) {
+        model.addAttribute("hello", "Hello Page");
+        return "helloPage";
+    }
+}
---
+++ src/main/java/web/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String greeting(Model model) {
+        model.addAttribute("title", "Home Page");
+        return "homePage";
+    }
+
+}
